c9293d086bbfc8b6e65ef0a3b02b28a81a81d266
Olesia <Kurcevskaaolesa@gmail.com>
2023-11-21 15:11:28 +0300

    1

---
+++ src/by/academy1/HomeWork/Ono.java
@@ -0,0 +1,17 @@
+package by.academy1.HomeWork;
+
+public class Ono {
+    public static void main(String[] args) {
+        int x = 0;
+
+        if (x>0) {
+            System.out.println(x  +" является положительным" );
+        } else if (x<0) {
+            System.out.println(x + " является отрицательным");
+        }else {
+            System.out.println(x + " является нулем");
+
+        }
+    }
+}
+
