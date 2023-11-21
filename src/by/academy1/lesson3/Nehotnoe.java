50dc474ac06b5cde6b50e9aa80eb1cc98ab09362
Olesia <Kurcevskaaolesa@gmail.com>
2023-11-21 15:14:40 +0300

    8

---
+++ src/by/academy1/lesson3/Nehotnoe.java
@@ -0,0 +1,13 @@
+package by.academy1.lesson3;
+
+public class Nehotnoe {
+    public static void main(String[] args) {
+        int a = 4;
+        if (a % 2 == 0) {
+            System.out.println(a + " четное ");
+        } else {
+            System.out.println(a + "  не четное ");
+
+        }
+
+    }}
